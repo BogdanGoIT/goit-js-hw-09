593fc35f169f75a31538587a009ba67d0418930d
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-06-09 00:26:53 +0300

    виконує таск 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,46 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+    start: document.querySelector('[data-start]'),
+    stop: document.querySelector('[data-stop]'),
+}
+
+console.log(refs.start);
+console.log(refs.stop);
+
+refs.start.addEventListener('click', onStartColor);
+refs.stop.addEventListener('click', onStopColor);
+
+let intervalId = null; 
+
+function onStartColor(e) {
+    e.target.disabled = true;
+    
+    intervalId = setInterval(() => {
+            
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+    refs.stop.disabled = false;
+}
+
+function onStopColor(e) {
+    clearInterval(intervalId);
+    refs.start.disabled = false;
+    e.target.disabled = true;
+}
+
+
+
+// function onLicenseChange(event) {
+    
+// // event.currentTarget - ссылка на текущий элемент
+//     console.log(event.currentTarget.checked);
+// //  disabled - у кнопки есть свойство
+//     console.log(refs.btn.disabled);
+
+// // когда чекбокс не выбран
+//     refs.btn.disabled = !event.currentTarget.checked;
+// }
