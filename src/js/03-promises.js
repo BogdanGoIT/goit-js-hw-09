4f013d18dc1aa03454082b5cbc333993c7f7650f
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-06-14 00:04:30 +0300

    виконує таск 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
+const refs = {
+  form: document.querySelector('.form'),
+  step: document.querySelector('input[name=step]'),
+  delay: document.querySelector('input[name=delay]'),
+  amount: document.querySelector('input[name=amount]'),
+}
+
+
+
+let promptCounter = 0;
+
+refs.form.addEventListener('submit', (e) => { 
+  e.preventDefault();
+  
+  let step = Number(refs.step.value);
+  let delay = Number(refs.delay.value);
+  const position = Number(refs.amount.value);
+
+  console.log(position);
+
+  const intervalId = setInterval(() => {
+    
+    if (promptCounter === position) {
+        console.log('нужно остановить интервал');
+        clearInterval(intervalId);
+        return;
+    } 
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+      .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+        promptCounter += 1;
+        delay += step;
+    
+  }, delay)
+  
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          if (shouldResolve) {
+          // Fulfill
+            resolve({ position, delay });
+          } else {
+          // Reject
+            reject({ position, delay });
+          }
+        }, delay)
+      })
 }
